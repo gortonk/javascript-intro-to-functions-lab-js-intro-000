e3d104be5a35046c2e1c069fbe6aae5e4c6a77c5
Katherine Gorton <kate.gorton25@gmail.com>
2019-06-21 14:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
-    return `I love you too.`
+    return `I love you, too.`
   } else if (string === string.toUpperCase()) {
-      return 'YES, INDEED!'
-  } else if (string === stringtoLowerCase()) {
+      return 'YES INDEED!'
+  } else if (string === string.toLowerCase()) {
       return `I can't hear you.`
   }
 }
